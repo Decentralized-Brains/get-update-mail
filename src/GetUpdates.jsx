78541f5777423028e453690151fad183b72fc92d
kumarbiplob336 <kumarbiplob336@gmail.com>
2023-04-21 03:52:17 +0600

    email done

--- src/GetUpdates.jsx
+++ src/GetUpdates.jsx
@@ -1,6 +1,29 @@
 import Logo from "./assets/logo.png";
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
 
 const GetUpdates = () => {
+  const form = useRef();
+  const [buttonText, setButtonText] = useState("GET UPDATES");
+  const SERVICE_ID = "service_z93v9pk"
+  const TEMPLATE_ID = "template_40sutlv"
+  const PUBLIC_KEY = "CLEL5gc6ucU5Raknv"
+
+  const sendEmail = (e) => {
+    setButtonText('Sending...')
+    e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+      .then((result) => {
+        window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+      }, (error) => {
+        console.log(error.text);
+      });
+    setButtonText("GET UPDATES")
+    e.target.reset();
+  };
+
   return (
     <div className="bg-updates h-screen w-screen flex justify-center items-center flex-col p-2 ">
       <div className="-mt-24">
@@ -18,24 +41,24 @@ const GetUpdates = () => {
       </p>
 
       <form
+        ref={form}
+        onSubmit={sendEmail}
         className="mt-10 flex flex-wrap justify-center items-center gap-2 "
       >
         <input
           type="text"
-          name="name"
-          id="name"
+          name="user_name"
           placeholder="Enter your name"
           className="bg-white  rounded-full py-2 px-4 w-full sm:w-48 md:w-60 lg:w-72"
         />
         <input
           type="email"
-          name="email"
-          id="email"
+          name="user_email"
           placeholder="Enter your email address"
           className="bg-white rounded-full py-2 px-4 w-full sm:w-48 md:w-60 lg:w-72"
         />
-        <button type={"submit"} className="bg-black hover:bg-white hover:text-black text-white  rounded-full py-2 px-6 duration-300">
-          GET UPDATES
+        <button type="submit" value="Send" className="bg-black hover:bg-white hover:text-black text-white  rounded-full py-2 px-6 duration-300">
+          {buttonText}
         </button>
       </form>
     </div>
